97a7f7b7afdae81aea5d3d24e49357b43d48d8a3
sykim <popokiss@gmail.com>
2026-03-26 13:58:49 +0900

    feat: TASK-03-004 카메라 개선 + 층 라벨 + FontManager (263/263)

---
+++ build3/tests/CMakeFiles/TowerTycoonTests.dir/DependInfo.cmake
@@ -0,0 +1,64 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/sykim/.openclaw/workspace/vse-platform/tests/PlaceholderTest.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/PlaceholderTest.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/PlaceholderTest.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/src/core/Bootstrapper.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/core/Bootstrapper.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/core/Bootstrapper.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/src/core/ConfigManager.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/core/ConfigManager.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/core/ConfigManager.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/src/core/ContentRegistry.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/core/ContentRegistry.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/core/ContentRegistry.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/src/core/EventBus.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/core/EventBus.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/core/EventBus.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/src/core/SimClock.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/core/SimClock.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/core/SimClock.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/src/domain/AgentSystem.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/domain/AgentSystem.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/domain/AgentSystem.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/src/domain/EconomyEngine.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/domain/EconomyEngine.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/domain/EconomyEngine.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/src/domain/GridSystem.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/domain/GridSystem.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/domain/GridSystem.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/src/domain/SaveLoadSystem.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/domain/SaveLoadSystem.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/domain/SaveLoadSystem.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/src/domain/StarRatingSystem.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/domain/StarRatingSystem.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/domain/StarRatingSystem.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/src/domain/TenantSystem.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/domain/TenantSystem.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/domain/TenantSystem.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/src/domain/TransportSystem.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/domain/TransportSystem.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/domain/TransportSystem.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/src/renderer/AnimationSystem.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/renderer/AnimationSystem.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/renderer/AnimationSystem.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/src/renderer/Camera.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/renderer/Camera.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/renderer/Camera.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/src/renderer/DebugPanel.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/renderer/DebugPanel.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/renderer/DebugPanel.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/src/renderer/FontManager.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/renderer/FontManager.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/renderer/FontManager.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/src/renderer/InputMapper.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/renderer/InputMapper.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/renderer/InputMapper.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/src/renderer/RenderFrameCollector.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/renderer/RenderFrameCollector.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/renderer/RenderFrameCollector.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/src/renderer/SDLRenderer.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/renderer/SDLRenderer.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/renderer/SDLRenderer.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/src/renderer/SpriteSheet.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/renderer/SpriteSheet.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/__/src/renderer/SpriteSheet.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/tests/test_AgentElevator.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/test_AgentElevator.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/test_AgentElevator.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/tests/test_AgentSystem.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/test_AgentSystem.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/test_AgentSystem.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/tests/test_Bootstrapper.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/test_Bootstrapper.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/test_Bootstrapper.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/tests/test_Camera.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/test_Camera.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/test_Camera.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/tests/test_ConfigManager.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/test_ConfigManager.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/test_ConfigManager.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/tests/test_ContentRegistry.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/test_ContentRegistry.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/test_ContentRegistry.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/tests/test_DebugInfo.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/test_DebugInfo.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/test_DebugInfo.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/tests/test_EconomyEngine.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/test_EconomyEngine.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/test_EconomyEngine.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/tests/test_FontManager.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/test_FontManager.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/test_FontManager.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/tests/test_GridSystem.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/test_GridSystem.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/test_GridSystem.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/tests/test_InputMapper.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/test_InputMapper.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/test_InputMapper.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/tests/test_Integration.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/test_Integration.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/test_Integration.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/tests/test_RenderAgent.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/test_RenderAgent.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/test_RenderAgent.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/tests/test_RenderFrame.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/test_RenderFrame.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/test_RenderFrame.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/tests/test_SaveLoad.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/test_SaveLoad.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/test_SaveLoad.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/tests/test_SimClock.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/test_SimClock.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/test_SimClock.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/tests/test_SpriteSheet.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/test_SpriteSheet.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/test_SpriteSheet.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/tests/test_StarRatingSystem.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/test_StarRatingSystem.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/test_StarRatingSystem.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/tests/test_TenantSystem.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/test_TenantSystem.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/test_TenantSystem.cpp.o.d"
+  "/home/sykim/.openclaw/workspace/vse-platform/tests/test_TransportSystem.cpp" "tests/CMakeFiles/TowerTycoonTests.dir/test_TransportSystem.cpp.o" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/test_TransportSystem.cpp.o.d"
+  "" "tests/TowerTycoonTests" "gcc" "tests/CMakeFiles/TowerTycoonTests.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build3/tests/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/sykim/.openclaw/workspace/vse-platform/tests
+# Build directory: /home/sykim/.openclaw/workspace/vse-platform/build3/tests
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+include("/home/sykim/.openclaw/workspace/vse-platform/build3/tests/TowerTycoonTests-b12d07c_include.cmake")
