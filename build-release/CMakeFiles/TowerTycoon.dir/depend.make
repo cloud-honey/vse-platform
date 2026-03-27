1f8f746b242b13698f77bc8aff5e3ab275eaa562
sykim <popokiss@gmail.com>
2026-03-27 17:14:15 +0900

    chore: build-release/ gitignore 추가 및 빌드 캐시 제거

--- build-release/CMakeFiles/TowerTycoon.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for TowerTycoon.
-# This may be replaced when dependencies are built.
--- build-release/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sykim/.openclaw/workspace/vse-platform/tests
-# Build directory: /home/sykim/.openclaw/workspace/vse-platform/build-release/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/sykim/.openclaw/workspace/vse-platform/build-release/tests/TowerTycoonTests-b12d07c_include.cmake")
